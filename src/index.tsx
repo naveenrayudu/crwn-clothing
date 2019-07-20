47a4a16baf4cc5c22b3db0ed9b8a59758e4fd8cf
Naveen Rayudu (110 Consulting) <v-narayu@microsoft.com>
2019-07-19 22:32:06 -0700

    Added persisitor

--- src/index.tsx
+++ src/index.tsx
@@ -6,22 +6,27 @@ import App from "./App";
 
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import rootReducer from './store/reducers/rootReducer';
-import logger from './store/middlewares/logger';
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+import rootReducer from "./store/reducers/rootReducer";
+import logger from "./store/middlewares/logger";
 import toasterMiddleware from "./store/middlewares/toaster-notification";
 
-const composeEnhancers = (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-const enhancer = composeEnhancers(
-  applyMiddleware(logger, toasterMiddleware)
-);
+const composeEnhancers =
+  (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const enhancer = composeEnhancers(applyMiddleware(logger, toasterMiddleware));
 
 const store = createStore(rootReducer, enhancer);
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -1,12 +1,23 @@
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+
 import currentUser from './users/userReducer';
 import cartReducer from './carts/cartReducer';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
 
 const rootReducer = combineReducers({
     user: currentUser,
     cart: cartReducer
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export type AppState = ReturnType<typeof rootReducer>
 
-export default rootReducer;
+export default persistedReducer;

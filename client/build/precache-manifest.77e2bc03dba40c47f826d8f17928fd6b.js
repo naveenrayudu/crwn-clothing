cb10dd41529749241d35e82c1fa7a4ae86a2a918
Naveen Rayudu (110 Consulting) <v-narayu@microsoft.com>
2019-07-27 00:29:37 -0700

    Added typescript for server

---
+++ client/build/precache-manifest.77e2bc03dba40c47f826d8f17928fd6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876696b56e63c8dc0e752bf93df17510",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c33d40537f17aaaaae7c",
+    "url": "/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "e5b0a6f92dcf42b8b9f8",
+    "url": "/static/css/main.ba404b8f.chunk.css"
+  },
+  {
+    "revision": "c33d40537f17aaaaae7c",
+    "url": "/static/js/2.3763672a.chunk.js"
+  },
+  {
+    "revision": "e5b0a6f92dcf42b8b9f8",
+    "url": "/static/js/main.71365ddd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);

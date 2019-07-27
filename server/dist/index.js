4976c16656fbec2879218cc92722b204ce01b266
Naveen Rayudu (110 Consulting) <v-narayu@microsoft.com>
2019-07-27 01:14:40 -0700

    Added build step

--- server/dist/index.js
+++ server/dist/index.js
@@ -8,8 +8,15 @@ const path_1 = __importDefault(require("path"));
 const app = express_1.default();
 const port = process.env.PORT || 3001;
 app.use(express_1.default.static(path_1.default.join(__dirname, "..", "..", "client", "build")));
+app.get("/api/paypal/payment", (req, res) => {
+    return res.send("Paypal transactions");
+});
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+// All non-found urls.
+app.get("*", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "..", "..", "client", "build", "index.html"));
 });
 app.listen(port, () => {
     console.log("Node server starteda");
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,8 +6,17 @@ const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "..", "..", "client", "build")));
 
+app.get("/api/paypal/payment", (req, res) => {
+    return res.send("Paypal transactions");
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+// All non-found urls.
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "..", "client", "build", "index.html"));
 });
 
 app.listen(port, () => {

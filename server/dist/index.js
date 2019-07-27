b255e4035429d263797c38f8a435e726692d682f
Naveen Rayudu (110 Consulting) <v-narayu@microsoft.com>
2019-07-27 00:54:57 -0700

    Added build step

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = express_1.default();
 const port = process.env.PORT || 3001;
+app.use(express_1.default.static(path_1.default.join(__dirname, "..", "..", "client", "public")));
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import path from "path";
+
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, "..", "..", "client", "public")));
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
